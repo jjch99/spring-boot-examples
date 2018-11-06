356d6784a21e6cdc2dde31295cb7db66e66ce86b
jjch99 <jjch99@163.com>
2018-11-06 12:47:39 +0800

    spring-boot-examples

--- spring-boot-example-config/spring-boot-example-config-apollo/src/main/java/org/example/config/spring/ApolloConfigurator.java
+++ spring-boot-example-config/spring-boot-example-config-apollo/src/main/java/org/example/config/spring/ApolloConfigurator.java
@@ -10,6 +10,9 @@ import com.ctrip.framework.apollo.spring.config.PropertySourcesConstants;
  * 初始化apollo需要的一些参数
  * 
  * @see com.ctrip.framework.apollo.spring.boot.ApolloApplicationContextInitializer
+ * @see com.ctrip.framework.apollo.internals.DefaultMetaServerProvider
+ * @see com.ctrip.framework.foundation.internals.provider.DefaultApplicationProvider
+ * @see com.ctrip.framework.foundation.internals.provider.DefaultServerProvider
  */
 public class ApolloConfigurator implements EnvironmentPostProcessor {
 
@@ -18,15 +21,19 @@ public class ApolloConfigurator implements EnvironmentPostProcessor {
 
         System.setProperty(PropertySourcesConstants.APOLLO_BOOTSTRAP_ENABLED, Boolean.TRUE.toString());
 
-        if (environment.getProperty("apollo.app.id") != null) {
+        // 如果VM参数和环境变量里没有特殊指定，则将程序配置中的相关参数设置进去
+
+        if (System.getProperty("app.id") == null && environment.getProperty("apollo.app.id") != null) {
             System.setProperty("app.id", environment.getProperty("apollo.app.id"));
         }
 
-        if (environment.getProperty("apollo.env") != null) {
+        if (System.getProperty("env") == null && System.getenv("ENV") == null
+                && environment.getProperty("apollo.env") != null) {
             System.setProperty("env", environment.getProperty("apollo.env"));
         }
 
-        if (environment.getProperty("apollo.idc") != null) {
+        if (System.getProperty("idc") == null && System.getenv("IDC") == null
+                && environment.getProperty("apollo.idc") != null) {
             System.setProperty("idc", environment.getProperty("apollo.idc"));
         }
 
