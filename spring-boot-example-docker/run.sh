7dcb2f6094c40e4609f5bcc9a773791202034a44
jjch99 <jjch99@163.com>
2019-05-05 16:26:19 +0800

    spring-boot-examples

--- spring-boot-example-docker/run.sh
+++ spring-boot-example-docker/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker run -p 8080:8081 spring-boot-example-docker:0.0.1-SNAPSHOT
+docker run -p 8080:8081 --name spring-boot-example-docker spring-boot-example-docker:0.1.0-SNAPSHOT
 
