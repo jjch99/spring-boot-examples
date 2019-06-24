be6f181703b0d5a9d4122209b25d32c410ecb222
jjch99 <jjch99@163.com>
2019-06-24 13:51:09 +0800

    spring-boot-examples

--- spring-boot-example-config/spring-boot-example-config-apollo-log4j2/src/main/java/org/example/config/ApolloLog4j2ConfigurationFactory.java
+++ spring-boot-example-config/spring-boot-example-config-apollo-log4j2/src/main/java/org/example/config/ApolloLog4j2ConfigurationFactory.java
@@ -16,6 +16,32 @@ import org.apache.logging.log4j.core.config.properties.PropertiesConfigurationBu
 import com.ctrip.framework.apollo.Config;
 import com.ctrip.framework.apollo.ConfigService;
 
+/**
+ * 采用Apollo来配置log4j2
+ * <p>
+ * Example:
+ * 
+ * <pre>
+ * status = debug
+ * name = PropertiesConfig
+ *
+ * filters = threshold
+ *
+ * filter.threshold.type = ThresholdFilter
+ * filter.threshold.level = debug
+ *
+ * appenders = console
+ *
+ * appender.console.type = Console
+ * appender.console.name = STDOUT
+ * appender.console.layout.type = PatternLayout
+ * appender.console.layout.pattern = %d{yyyy-MM-dd HH:mm:ss} %-5p %c{1}:%L ---- %m%n
+ *
+ * rootLogger.level = info
+ * rootLogger.appenderRefs = stdout
+ * rootLogger.appenderRef.stdout.ref = STDOUT
+ * </pre>
+ */
 @Plugin(name = "ApolloLog4j2ConfigurationFactory", category = ConfigurationFactory.CATEGORY)
 @Order(50)
 public class ApolloLog4j2ConfigurationFactory extends ConfigurationFactory {
