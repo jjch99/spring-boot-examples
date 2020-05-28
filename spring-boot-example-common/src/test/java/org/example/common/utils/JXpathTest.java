697734c40b667a1a7909b7cd20e53b924b947fac
jjch99 <jjch99@163.com>
2020-05-28 18:43:31 +0800

    spring-boot-examples

---
+++ spring-boot-example-common/src/test/java/org/example/common/utils/JXpathTest.java
@@ -0,0 +1,25 @@
+package org.example.common.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.jxpath.JXPathContext;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMultimap;
+
+public class JXpathTest {
+
+    @Test
+    public void testCase1() throws Exception {
+        Map map = new HashMap();
+        map.put("a", 1);
+        map.put("b", ImmutableMultimap.builder().put("b1", "b1").build());
+        System.out.println(map);
+
+        JXPathContext context = JXPathContext.newContext(map);
+        String b1 = (String) context.getValue("//b/b1");
+        System.out.println(b1);
+    }
+
+}
---
+++ spring-boot-example-common/src/test/java/org/example/common/utils/JsonPathTest.java
@@ -0,0 +1,8 @@
+package org.example.common.utils;
+
+/**
+ * https://github.com/json-path/JsonPath
+ */
+public class JsonPathTest {
+
+}
