080bd4e96c27903799e2000d788f2e22cdff772d
jjch99 <jjch99@163.com>
2020-06-19 19:50:39 +0800

    spring-boot-examples

--- spring-boot-example-common/src/test/java/org/example/common/utils/JacksonTest.java
+++ spring-boot-example-common/src/test/java/org/example/common/utils/JacksonTest.java
@@ -3,16 +3,21 @@ package org.example.common.utils;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * https://www.cnblogs.com/three-fighter/p/12945142.html
  */
+@Slf4j
 public class JacksonTest {
 
     @Test
@@ -26,15 +31,47 @@ public class JacksonTest {
 
         ObjectMapper mapper = new ObjectMapper();
         String jsonStr = mapper.writeValueAsString(map);
-        System.out.println(jsonStr);
+        log.info(jsonStr);
 
         Map map1 = mapper.readValue(jsonStr, HashMap.class);
-        System.out.println(map1);
+        log.info(map1.toString());
 
         JsonNode jsonNode = mapper.readTree(jsonStr);
-        System.out.println(jsonNode);
-        System.out.println(jsonNode.get("a").toString());
-        System.out.println(jsonNode.findValue("b"));
+        log.info(jsonNode.toPrettyString());
+        log.info("a=" + jsonNode.get("a").toString());
+        log.info("c1=" + jsonNode.findValue("c1"));
+        log.info("/c/c1=" + jsonNode.at("/c/c1").toString());
+
+        updateValue(jsonNode, "/c/c1", "Hello");
+        log.info(jsonNode.toPrettyString());
+    }
+
+    /**
+     * 修改指定节点的值
+     * 
+     * @param root
+     * @param path
+     * @param value
+     * @return
+     */
+    public static boolean updateValue(JsonNode root, String path, String value) {
+        String[] elements = StringUtils.split(path, '/');
+        JsonNode parent = root;
+        JsonNode node = root;
+        String nodeName = null;
+        for (String element : elements) {
+            if (!node.has(element)) {
+                return false;
+            }
+            nodeName = element;
+            parent = node;
+            node = parent.get(nodeName);
+        }
+
+        ObjectNode parentNode = (ObjectNode) parent;
+        parentNode.put(nodeName, value);
+        return true;
+
     }
 
 }
