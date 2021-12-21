e76abe23f5daebde564a7e7c9b4c028ee76d1adf
jjch99 <jjch99@163.com>
2021-12-21 11:55:57 +0800

    spring-boot-examples

--- spring-boot-example-aop/src/main/java/org/example/aspect/ApiInterceptor.java
+++ spring-boot-example-aop/src/main/java/org/example/aspect/ApiInterceptor.java
@@ -1,9 +1,12 @@
 package org.example.aspect;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,6 +14,7 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 
 @Aspect
 @Component
@@ -24,23 +28,35 @@ public class ApiInterceptor {
 
     @Around("apiMethodAspect()")
     public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
-
-        Object[] args = joinPoint.getArgs();
-        log.info("arg num: " + args.length + ", args: " + Arrays.asList(args));
-
-        Object ret = null;
-        try {
-            ret = joinPoint.proceed();
-
-            Gson gson = new Gson();
-            log.info("ret: {}", gson.toJson(ret));
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            // 此处如果屏蔽了异常不抛出去，Controller里的@ExceptionHandler就收不到异常了
-            throw e;
+        Gson gson;
+        if (log.isDebugEnabled()) {
+            gson = new GsonBuilder().setPrettyPrinting().create();
+        } else {
+            gson = new Gson();
         }
 
-        return ret;
+        Object[] args = joinPoint.getArgs();
+        List argList = new ArrayList(args.length);
+        for (Object arg : args) {
+            if (arg != null && arg.getClass().getName().startsWith("javax.servlet.")) {
+                argList.add(arg.toString());
+            } else {
+                argList.add(arg);
+            }
+        }
+        log.info("invoke {}, args: {}", joinPoint.getSignature(), gson.toJson(argList));
+
+        try {
+            Object ret = joinPoint.proceed();
+
+            log.info("ret: {}", gson.toJson(ret));
+
+            return ret;
+
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw e;
+        }
     }
 
 }
