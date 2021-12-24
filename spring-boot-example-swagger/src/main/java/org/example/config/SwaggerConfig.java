71732094d083344071fa7f2d52afd7ce816cc3cb
jjch99 <jjch99@163.com>
2021-12-24 13:41:28 +0800

    spring-boot-examples

--- spring-boot-example-swagger/src/main/java/org/example/config/SwaggerConfig.java
+++ spring-boot-example-swagger/src/main/java/org/example/config/SwaggerConfig.java
@@ -1,22 +1,23 @@
 package org.example.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@ConditionalOnProperty(name = "swagger.enabled", havingValue = "true")
 public class SwaggerConfig {
 
     @Bean
-    public Docket createRestApi() {
+    public Docket enableSwagger() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .enable(true)
@@ -28,9 +29,8 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("XXX Rest Server")
-                .description("XXXRest接口")
-                .contact(new Contact("contract", "url", "email"))
+                .title("Demo Server")
+                .description("Demo API")
                 .version("1.0")
                 .build();
     }
--- spring-boot-example-swagger/src/main/java/org/example/domain/HelloRequest.java
+++ spring-boot-example-swagger/src/main/java/org/example/domain/HelloRequest.java
@@ -5,8 +5,6 @@ import lombok.Data;
 @Data
 public class HelloRequest {
 
-    private Long id;
-
     private String name;
 
 }
