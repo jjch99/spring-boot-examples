5de97cf86f6c3d378dbe835a78d16ad3feae3183
jjch99 <jjch99@163.com>
2024-12-23 11:39:13 +0800

    spring-boot-examples

--- spring-boot-example-web/src/main/java/org/example/config/RestTemplateConfig.java
+++ spring-boot-example-web/src/main/java/org/example/config/RestTemplateConfig.java
@@ -1,9 +1,9 @@
 package org.example.config;
 
+import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.http.client.config.RequestConfig;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
@@ -14,9 +14,6 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
-    @Autowired
-    private RequestConfig requestConfig;
-
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate(httpComponentsClientHttpRequestFactory());
@@ -24,10 +21,14 @@ public class RestTemplateConfig {
 
     @Bean
     public ClientHttpRequestFactory httpComponentsClientHttpRequestFactory() {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
+        RequestConfig config = RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofMilliseconds(3000))
+                .setResponseTimeout(Timeout.ofMilliseconds(5000))
+                .build();
+        CloseableHttpClient httpclient = HttpClients.custom().setDefaultRequestConfig(config).build();
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpclient);
-        factory.setConnectionRequestTimeout(requestConfig.getConnectionRequestTimeout());
-        factory.setConnectTimeout(requestConfig.getConnectTimeout());
+        factory.setConnectionRequestTimeout(config.getConnectionRequestTimeout().toMillisecondsIntBound());
+        factory.setConnectTimeout(config.getConnectionRequestTimeout().toMillisecondsIntBound());
         return factory;
     }
 
