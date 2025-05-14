f2ae2c8c4a1cbbca80188780510734cfc61f1139
jjch99 <jjch99@163.com>
2025-05-14 19:48:55 +0800

    spring-boot-examples

--- run.sh
+++ run.sh
@@ -5,14 +5,13 @@ cd $WORK_DIR
 echo "workdir is: $WORK_DIR"
 
 . ${WORK_DIR}/setenv.sh
-alias mvn=$WORK_DIR/mvnw
 
 set -e
 
 # Function to find Spring Boot modules recursively
 find_boot_modules() {
-  find . -type f -name pom.xml | while read -r pom; do
-    if grep -q "spring-boot-maven-plugin" "$pom"; then
+  find . -mindepth 2 -type f -name pom.xml | while read -r pom; do
+    if ! grep -q "<packaging>pom</packaging>" "$pom"; then
       module_path=$(dirname "$pom")
       module_name="${module_path#./}"
       echo "$module_name"
@@ -47,16 +46,18 @@ if [ ! -f "$POM_FILE" ]; then
   exit 1
 fi
 
-# Check if spring-boot-maven-plugin is configured
-if ! grep -q "spring-boot-maven-plugin" "$POM_FILE"; then
-  echo "Error: spring-boot-maven-plugin is not configured in '$MODULE_NAME'."
+# Check if a pom module
+if grep -q "<packaging>pom</packaging>" "$POM_FILE"; then
+  echo "Error: '$MODULE_NAME' is a pom module."
   exit 1
 fi
 
+$WORK_DIR/mvnw -v
+
 # Build the module before running
 echo "Building module '$MODULE_NAME' (skip tests)..."
-mvn -pl "$MODULE_NAME" -am package -DskipTests -e -s ${WORK_DIR}/.mvn/settings.xml
+$WORK_DIR/mvnw -pl "$MODULE_NAME" -am package -DskipTests -e -s ${WORK_DIR}/.mvn/settings.xml
 
 # Run the Spring Boot application
 echo "Running module: $MODULE_NAME"
-mvn -pl "$MODULE_NAME" spring-boot:run -e -s ${WORK_DIR}/.mvn/settings.xml
+$WORK_DIR/mvnw -pl "$MODULE_NAME" spring-boot:run -e -s ${WORK_DIR}/.mvn/settings.xml
